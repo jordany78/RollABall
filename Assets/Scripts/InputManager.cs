b90f5e6db2d17ca0ce29bbd626baa614dea37a5d
Jordan Truong <jordantruong26@gmail.com>
2025-01-16 13:52:53 -0800

    added debug log for keys pressed

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -12,23 +12,30 @@ public class InputManager : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             inputVector += Vector2.up;
+            Debug.Log("up");
         }
         if (Input.GetKey(KeyCode.S))
         {
             inputVector += Vector2.down;
+            Debug.Log("down");
         }
         if (Input.GetKey(KeyCode.D))
         {
             inputVector += Vector2.right;
+            Debug.Log("right");
         }
         if (Input.GetKey(KeyCode.A))
         {
             inputVector += Vector2.left;
+            Debug.Log("left");
         }
         if (Input.GetKey(KeyCode.Space))
         {
             jump = true;
         }
         OnMove?.Invoke(inputVector, jump);
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        Debug.Log("Resultant Vector: " + inputVector);
+        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
     }
 }
